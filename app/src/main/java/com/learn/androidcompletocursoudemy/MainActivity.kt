a7a9b201d40bd6369c7901f483e2a4f95a598264
Marcos Henrique <80632969+midnightbr@users.noreply.github.com>
2025-04-17 00:08:07 -0300

    - Códificando método para que quando o botão seja clicado ele retorne um número aleatorio no textView

--- app/src/main/java/com/learn/androidcompletocursoudemy/MainActivity.kt
+++ app/src/main/java/com/learn/androidcompletocursoudemy/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.learn.androidcompletocursoudemy
 
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,5 +20,11 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+
+    fun random(view: View) {
+        val textResult = findViewById<TextView>(R.id.text_result)
+        val number = Random.nextInt(51)
+        textResult.setText("Resultado: ${number}")
     }
 }
