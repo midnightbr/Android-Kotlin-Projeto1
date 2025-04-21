ba5551dbcc0e0106dfa733d0e90b506aa1400b22
Marcos Henrique <80632969+midnightbr@users.noreply.github.com>
2025-04-21 16:20:16 -0300

    - Exemplos de sobrecarga de métodos

--- app/src/main/java/com/learn/androidcompletocursoudemy/ProgramacaoOrientadaObjetos.kt
+++ app/src/main/java/com/learn/androidcompletocursoudemy/ProgramacaoOrientadaObjetos.kt
@@ -1,5 +1,7 @@
 package com.learn.androidcompletocursoudemy
 
+import android.app.admin.TargetUser
+
 class Player {
     var kart: String = ""
     var pneu: String = ""
@@ -7,6 +9,15 @@ class Player {
 
     fun acelerar(acelerador: Int ) {
         println("Acelerando a $acelerador%")
+    }
+
+    // Sobrecarga
+    fun logar(email: String, senha: String) {
+        println("Usuário logado com email: $email e senha $senha")
+    }
+
+    fun logar(username: String, senha: Int) {
+        println("Usuário logado com username: $username e senha $senha")
     }
 }
 
