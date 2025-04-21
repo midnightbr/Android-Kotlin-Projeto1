d9bb0350b18bb76389a796792069ec7694d67191
Marcos Henrique <80632969+midnightbr@users.noreply.github.com>
2025-04-21 03:42:02 -0300

    - Explicando funções Inline (funções simplificadas)

--- app/src/main/java/com/learn/androidcompletocursoudemy/ProgramacaoFuncional.kt
+++ app/src/main/java/com/learn/androidcompletocursoudemy/ProgramacaoFuncional.kt
@@ -10,8 +10,17 @@ fun somar(): Int {
     return 5 + 7
 }
 
+// Função Inline sem retorno
+fun exibirSubtracao() = println(25 - 7)
+
+// Função Inline com retorno
+fun subtrair(valorA: Int, valorB: Int): Int = valorA - valorB
+
 fun main() {
     soma()
     val result = somar()
     println(result)
+    exibirSubtracao()
+    val resultSubtracao = subtrair(25, 10)
+    println(resultSubtracao)
 }
