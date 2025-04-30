49514a823f5a5f506ba20ba8e2304ec50311f16d
Marcos Henrique <80632969+midnightbr@users.noreply.github.com>
2025-04-29 23:59:40 -0300

    - Implementando classes para teste no arquivo main

--- app/src/main/java/com/learn/androidcompletocursoudemy/heranca/Main.kt
+++ app/src/main/java/com/learn/androidcompletocursoudemy/heranca/Main.kt
@@ -1,5 +1,9 @@
 package com.learn.androidcompletocursoudemy.heranca
 
 fun main() {
-
+    val dog = Dog("preto", "medio", 15.0)
+    val bird = Bird("azul", "pequeno", 0.150)
+    dog.run()
+    dog.sleep()
+    bird.launch()
 }
