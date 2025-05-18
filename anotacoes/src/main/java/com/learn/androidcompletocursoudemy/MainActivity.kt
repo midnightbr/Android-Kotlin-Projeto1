f12a31263b07842ef0d18f979db3736898a59529
Marcos Henrique <marcosriderbr@outlook.com>
2025-05-18 17:09:15 -0300

    - Aula explicando sobre Activity, como chamar (abrir) uma activity e pilhas de Activity's

--- anotacoes/src/main/java/com/learn/androidcompletocursoudemy/MainActivity.kt
+++ anotacoes/src/main/java/com/learn/androidcompletocursoudemy/MainActivity.kt
@@ -1,29 +1,38 @@
 package com.learn.androidcompletocursoudemy
 
+import android.content.Intent
 import android.os.Bundle
-import android.view.View
-import android.widget.TextView
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+    lateinit var btnOpen: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        btnOpen = findViewById(R.id.btnTelaSorteio)
+        btnOpen.setOnClickListener {
+            /**
+             * Passando o contexto e a classe de destino da tela que será aberta.
+             * Note que para passar a classe resposanvel pela tela, é utilizado
+             * após o nome da classe "::class.java".
+             */
+            val intent = Intent(this, SorteioActivity::class.java)
+
+            // Iniciar uma activity/tela de sorteio
+            startActivity(intent)
+        }
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-    }
-
-    fun random(view: View) {
-        val textResult = findViewById<TextView>(R.id.text_result)
-        val number = Random.nextInt(51)
-        textResult.setText("Resultado: ${number}")
     }
 }
--- anotacoes/src/main/java/com/learn/androidcompletocursoudemy/SorteioActivity.kt
+++ anotacoes/src/main/java/com/learn/androidcompletocursoudemy/SorteioActivity.kt
@@ -1,20 +1,39 @@
 package com.learn.androidcompletocursoudemy
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import kotlin.random.Random
 
-class DetailsActivity : AppCompatActivity() {
+class SorteioActivity : AppCompatActivity() {
+    lateinit var btnClose: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_details)
+        setContentView(R.layout.activity_sorteio)
+
+        btnClose = findViewById(R.id.btnClose)
+        btnClose.setOnClickListener {
+            // Fechar tela/activity
+            finish()
+        }
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+
+    fun random(view: View) {
+        val textResult = findViewById<TextView>(R.id.text_result)
+        val number = Random.nextInt(51)
+        textResult.setText("Resultado: ${number}")
     }
 }
