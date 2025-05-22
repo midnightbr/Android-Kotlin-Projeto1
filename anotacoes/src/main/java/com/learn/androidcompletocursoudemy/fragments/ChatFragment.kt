fced9e20c2a617dc9a71093255ad01c79ea4cbed
Marcos Henrique <marcosriderbr@outlook.com>
2025-05-22 16:41:13 -0300

    - Ciclo de vida do fragment view

--- anotacoes/src/main/java/com/learn/androidcompletocursoudemy/fragments/ChatFragment.kt
+++ anotacoes/src/main/java/com/learn/androidcompletocursoudemy/fragments/ChatFragment.kt
@@ -1,6 +1,8 @@
 package com.learn.androidcompletocursoudemy.fragments
 
+import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -15,6 +17,8 @@ class ChatFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        Log.i("ciclo_vida", "Fragment onCreateView")
+
         val view = inflater.inflate(
             R.layout.fragment_chat,
             container,
@@ -23,4 +27,56 @@ class ChatFragment : Fragment() {
 
         return view
     }
+
+    // Ciclo de vida dos Fragmentos
+    override fun onAttach(context: Context) {
+        Log.i("ciclo_vida", "Fragment onAttach")
+        super.onAttach(context)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        Log.i("ciclo_vida", "Fragment onCreate")
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        Log.i("ciclo_vida", "Fragment onViewCreated")
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onStart() {
+        Log.i("ciclo_vida", "Fragment onStart")
+        super.onStart()
+    }
+
+    override fun onResume() {
+        Log.i("ciclo_vida", "Fragment onResume")
+        super.onResume()
+    }
+
+    override fun onPause() {
+        Log.i("ciclo_vida", "Fragment onPause")
+        super.onPause()
+    }
+
+    override fun onStop() {
+        Log.i("ciclo_vida", "Fragment onStop")
+        super.onStop()
+    }
+
+    override fun onDestroyView() {
+        Log.i("ciclo_vida", "Fragment onDestroyView")
+        super.onDestroyView()
+    }
+
+    override fun onDestroy() {
+        Log.i("ciclo_vida", "Fragment onDestroy")
+        super.onDestroy()
+    }
+
+    override fun onDetach() {
+        Log.i("ciclo_vida", "Fragment onDetach")
+        super.onDetach()
+    }
+
 }
